083a256a7ee3a2f9493229e63067cf422bb6cd47
DENISE.DL <denise.dl@hotmail.it>
2024-03-14 10:18:17 +0100

    aggiunto costruttore,getter,setter

--- src/main/java/com/database/database/test1/esercizio1/entities/Classes.java
+++ src/main/java/com/database/database/test1/esercizio1/entities/Classes.java
@@ -13,4 +13,37 @@ public class Classes {
     private String title;
     @Column(nullable = false)
     private String description;
+
+    public Classes(Long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Classes() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/database/database/test1/esercizio1/entities/Student.java
+++ src/main/java/com/database/database/test1/esercizio1/entities/Student.java
@@ -18,6 +18,45 @@ public class Student {
     @Column(unique = true,length = 100,nullable = false)
     private String email;
 
+    public Student(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
+    public Student() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
